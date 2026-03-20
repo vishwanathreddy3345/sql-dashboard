66322c198563bff70a4469563faa1343b23f60ad
vishwanathreddy3345 <vishwanathreddy3345@gmail.com>
2026-03-20 10:30:26 +0530

    Fixed port issue for deployment

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 from werkzeug.security import check_password_hash
 
+import os
+
 app = Flask(__name__)
 app.secret_key = "secret123"
 
@@ -181,4 +183,5 @@ def delete_employee(id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
